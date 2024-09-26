c059abc712db127885f1d0a3183497705e1802fb
FrancoisLGRND <lgrdpaco@gmail.com>
2024-09-26 19:24:11 +0200

    ajout matrice map

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "graphics.c"
 
 int main(){
+    int columns = 7;
+    int rows = 6;
     printf("hello");
+    short **map = malloc(sizeof(short) * rows);
+    for(int i = 0; i <rows; i++){
+        map[i] = malloc(sizeof(short) * columns);
+    }
+
+
     return -1;
 }
