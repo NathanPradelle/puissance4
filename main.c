b3103dc2a13b5751d5ee5a41272bd9d69c34c328
NathanPradelle <nathan.pradelle77@gmail.com>
2024-09-26 21:26:24 +0200

    createMap and User

--- main.c
+++ main.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "graphics.c"
+#include "structure.c"
+
 
 int main(){
-    int columns = 7;
-    int rows = 6;
-    printf("hello");
-    short **map = malloc(sizeof(short) * rows);
-    for(int i = 0; i <rows; i++){
-        map[i] = malloc(sizeof(short) * columns);
+    
+    char *firstUserName = malloc(sizeof(char) * 20);
+    char *secondUserName = malloc(sizeof(char) * 20);
+
+    Map *map = InitializeMap(4, 6, 7); 
+    
+    for (int i = 0; i<6; i++){
+        for (int j = 0; j<7; j++){
+            printf("%d", map->grid[i][j]);
+        }
+        printf("\n");
     }
-
-
-    return -1;
+    
+    printf("Nom du User1 : ");
+    scanf("%s", firstUserName);
+    printf("\nNom du User2 : ");
+    scanf("%s", secondUserName);
+    
+    User *firstUser = createUser(firstUserName, 0, 0, 'x');
+    User *secondUser = createUser(secondUserName, 1, 1, '0');
+    
+    return 0;
 }
--- structure.c
+++ structure.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char *name;
     short order;
     short color;
     char pawn;
+
 } User;
 
 typedef struct {
     int rows;
     int columns;
     int rule;
+    short **grid;
 } Map;
 
 User *createUser(char *name, short order, short color, char pawn){
 
     User *user = malloc(sizeof(User));
 
-    char *Username = malloc(sizeof(char) * (strlen + 1));
+    char *Username = malloc(sizeof(char) * (strlen(name) + 1));
     strcpy(Username, name);
 
     user->name = Username;
@@ -29,11 +32,21 @@ User *createUser(char *name, short order, short color, char pawn){
     return user;
 }
 
-Map *createMap(int rows, int columns, int rule){
+Map *InitializeMap(int rule, int rows, int columns){
 
     Map *map = malloc(sizeof(Map));
 
-    map->columns = rows;
+    map->rows = rows;
     map->columns = columns;
-    map->columns = rule;
+    map->rule = rule;
+
+    short **grid = (short **) malloc(sizeof(short*) * rows);
+    
+        for(int i = 0; i < rows; i++){
+            grid[i] = (short *) calloc(columns, sizeof(short));
+        }
+    
+    map->grid = grid;
+
+    return map;
 }
